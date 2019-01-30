db93d6c786d3f320fe367dc5473cc796a1a0d17a
Ana Flores <anafrs26@gmail.com>
2019-01-29 22:57:34 -0600

    actualizado

--- src/cipher.js
+++ src/cipher.js
@@ -4,11 +4,30 @@ function encriptar (texto, desplazamientos) {
         process.stdout.write("Valores permitidos solo de 1 a 33");
         return;
     }
+    let secreto = "";
     for (let indiceLetraActual = 0; indiceLetraActual<texto.length; indiceLetraActual++) {
-    const codigoASCII = (texto.charCodeAt(indiceLetraActual)-65+desplazaminetos)%26+65;
-    if (texto[indiceLetraActual] == " ") process.stdout.write(" ");
-    else process.stdout.write(String.fromCharCode(codigoASCII));
+        const codigoASCII = (texto.charCodeAt(indiceLetraActual)-65+desplazamientos)%26+65;
+        if (texto[indiceLetraActual] == " ") secreto = secreto + " ";
+        else secreto= secreto + String.fromCharCode(codigoASCII);
   }
-  
-  document.getElementById("decipherSentence").innerHTML = "Click en boton para descifrar";
+  return secreto;
+}
 
+  function desencriptar (texto, desplazamientos) {
+    if (desplazamientos > 33) {
+        process.stdout.write("Valores permitidos solo de 1 a 33");
+        return;
+    }
+    let revelado = "";
+    for (let indiceLetraActual = 0; indiceLetraActual<texto.length; indiceLetraActual++) {
+        const codigoDesencriptado = (texto.charCodeAt(indiceLetraActual)+ 65-desplazamientos)%26+65;
+        if (texto[indiceLetraActual] == " ") revelado = revelado +" ";
+        else revelado = revelado + String.fromCharCode(codigoDesencriptado);
+    }
+    return revelado;
+}
+
+let despl = 33
+let mySecreto = encriptar("ANA LAURA", despl)
+process.stdout.write(mySecreto + "\n")
+process.stdout.write(desencriptar(mySecreto, despl))
